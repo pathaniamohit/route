07636fb61c7a58d288e02e01bbedb52c6f5ee0d9
pathaniamohit <pathaniamohit0711raj@gmail.com>
2023-06-07 14:12:44 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/': (context) => HomePage(),
+      '/landingPage': (context) => SecondHome(),
+    },
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/landingPage');
+          },
+          child: Text('second home'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondHome extends StatelessWidget {
+  const SecondHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Welcome to my second home'),
+        ),
+      ),
+    );
+  }
+}
